81d7fa87d0c2b11ea031cf17299565243776b548
Elene-Gurgenidze <elenegurgenidze81@gmail.com>
2026-03-28 13:32:06 +0400

    final project update

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         <Route path="/ZX7SPEAKER" element={<ZX7SPEAKER />} />
         <Route path="/YX1WIRELESS" element={<YX1WIRELESS />} />
 
-
+      
       </Routes>
     </>
   );
